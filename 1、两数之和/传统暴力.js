beb60047fe0501b401b44e1127096eafa6cde7d7
Y-Q-Y-SJ <13864929089@163.com>
2026-01-19 15:24:07 +0800

    代码

---
+++ 1、两数之和/传统暴力.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) 
+{
+    let hash = new Map()
+    for(let i= 0; i<nums.length;i++)
+    {
+        hash.set(nums[i],i)
+    }
+    for(let i = 0;i<nums.length;i++)
+    {
+        if(hash.get(target-nums[i]) === undefined)
+            continue
+        else if(hash.get(target-nums[i]) === i)
+            continue
+        else
+            return [i,hash.get(target-nums[i])]
+    }
+};
